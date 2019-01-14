3768f57118d6e6e565a0b5008902db69b0325bd8
Aaron_G <gauntta@gmail.com>
2019-01-13 19:51:34 -0600

    Added additional JS commentary

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,13 @@ var loses = 0;
 var currentScore = 0;
 
 
-//Create inital random number
-function randomInt(min,max) // min and max included
-{
+//Create inital random number...Ran during the reRoll function
+function randomInt(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function reRoll(){
+//fuction that will reset the game parameters...Ran if the WinLose fuction is triggered.
+function reRoll(){ 
 randomNumber = randomInt(19,120);
 $("#randomNumberField").text(randomNumber);
 
@@ -21,23 +21,27 @@ button2Value = randomInt(1,12);
 button3Value = randomInt(1,12);
 button4Value = randomInt(1,12);
 
+//logs the crystals values for testing purposes
 console.log(button1Value);
 console.log(button2Value);
 console.log(button3Value);
 console.log(button4Value);
+
+//resets the current score
 currentScore = 0;
 $("#currentScore").text(currentScore);
 }
+
 reRoll();
 
-//Win / Lose function determines in the click will result in a win, or a bust, and make the corresponding updates
+//Win / Lose function determines if the click will result in a win, or a bust, and make the corresponding updates...Runs when the buttons are pressed.
 function WinLose(){
-    if (currentScore === randomNumber) {
+    if (currentScore === randomNumber) {  //UPdates the scoreboard and resets the game if a win is detected
         $("#winOrLose").text("You Win!");
         wins++;
         $("#wins").text("Wins: " + wins);
         reRoll();
-    } else if (currentScore > randomNumber) {
+    } else if (currentScore > randomNumber) { //Updates the scoreboard and resets the game if a lose is dectected
         $("#winOrLose").text("You Lose!");
         loses++;
         $("#loses").text("Loses: " + loses);
